3555a9ceab2b834dbe1d5d157ff3c06b5693c7bf
Moises Rodrigues <moisesabreurodrigues@gmail.com>
2022-04-02 18:00:20 -0300

    grava os dados no arquivo

--- index.php
+++ index.php
@@ -6,4 +6,10 @@ use TccUnifametro\Entities\{Expandir, Extensao};
 $cursosDeExtensao = (new Extensao())->run();
 $cursosDoExpandir = (new Expandir())->run();
 
-dump($cursosDeExtensao, $cursosDoExpandir);
+$data = collect(['extensao' => $cursosDeExtensao, 'expandir' => $cursosDoExpandir])->toArray();
+
+$arquivo = 'data.json';
+$json    = json_encode($data);
+$file    = fopen(__DIR__ . '/' . $arquivo, 'w');
+fwrite($file, $json);
+fclose($file);
